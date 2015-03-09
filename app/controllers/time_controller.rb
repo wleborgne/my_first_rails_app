44eca969d5b3e3e516305989abfd1d631928e4b8
Bill LeBorgne <bill@leborgne.name>
2015-03-09 17:05:16 -0600

    [#89941620] Add time reporting page
    Added new route for time/now
    Added time_controller
    Added now view with formatted time string

---
+++ app/controllers/time_controller.rb
@@ -0,0 +1,5 @@
+class TimeController < ApplicationController
+  def now
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+  get 'time/now' => 'time#now'
 end
